a5f4bed3c8ffd6bb0743ddc0140bef80c42a9f0f
Richard Alhama <richard.alhama@gmai.com>
2024-03-01 01:41:56 +0000

    refactored the authentication middleware

--- routes/OSCPApplicationRoutes.js
+++ routes/OSCPApplicationRoutes.js
@@ -3,10 +3,17 @@ const router = express.Router();
 const OSCPApplicationController = require('../controllers/OSCPApplicationController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-router.post('/', OSCPApplicationController.createOSCPApplication);
-router.get('/', OSCPApplicationController.getOSCPApplications);
-router.get('/:id', OSCPApplicationController.getOSCPApplicationById);
-router.put('/:id', OSCPApplicationController.updateOSCPApplication);
+router.post('/', authMiddleware, OSCPApplicationController.createOSCPApplication);
+router.get('/', authMiddleware, OSCPApplicationController.getOSCPApplications);
+router.get('/:id', authMiddleware, OSCPApplicationController.getOSCPApplicationById);
+router.put('/:id', authMiddleware, OSCPApplicationController.updateOSCPApplication);
+//router.delete('/oscpapplications/:id', auth, applicationController.deleteApplication);
+
+// No authentication 
+//router.post('/',s OSCPApplicationController.createOSCPApplication);
+//router.get('/', OSCPApplicationController.getOSCPApplications);
+//router.get('/:id', OSCPApplicationController.getOSCPApplicationById);
+//router.put('/:id', OSCPApplicationController.updateOSCPApplication);
 //router.delete('/oscpapplications/:id', auth, applicationController.deleteApplication);
 
 module.exports = router;
