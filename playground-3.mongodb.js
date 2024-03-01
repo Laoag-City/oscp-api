fdcf6c5d61f7c1a645a2a1a3052fe8bc4d4443a9
Richard Alhama <richard.alhama@gmail.com>
2024-03-01 12:29:43 +0000

    change playground 3

--- playground-3.mongodb.js
+++ playground-3.mongodb.js
@@ -10,57 +10,57 @@ use('oscp');
 db.getCollection('users').insertMany([
   {
     username: 'kechie',
-    password: '$2a$08$8g/.DFmReWOtKxE0b4OvneXn5PXttd94GjtS7nJCRl7M4QeNZQoEe',
+    password: '$2a$12$YIctUUQJtNO1aRW8Q2GaNOeq5WYiVdT2EibR6TLj2PgSx73SKpQpa',
     userrole: 'oscpadmin',
   },
   {
     username: 'oscpcmo',
-    password: '$2a$08$8g/.DFmReWOtKxE0b4OvneXn5PXttd94GjtS7nJCRl7M4QeNZQoEe',
+    password: '$2a$12$YIctUUQJtNO1aRW8Q2GaNOeq5WYiVdT2EibR6TLj2PgSx73SKpQpa',
     userrole: 'oscpcmo',
   },
   {
     username: 'oscpzoning',
-    password: '$2a$08$8g/.DFmReWOtKxE0b4OvneXn5PXttd94GjtS7nJCRl7M4QeNZQoEe',
+    password: '$2a$12$YIctUUQJtNO1aRW8Q2GaNOeq5WYiVdT2EibR6TLj2PgSx73SKpQpa',
     userrole: 'oscpzoning',
   },
   {
     username: 'oscpbfp',
-    password: '$2a$08$8g/.DFmReWOtKxE0b4OvneXn5PXttd94GjtS7nJCRl7M4QeNZQoEe',
+    password: '$2a$12$YIctUUQJtNO1aRW8Q2GaNOeq5WYiVdT2EibR6TLj2PgSx73SKpQpa',
     userrole: 'oscbfp',
   },
   {
     username: 'oscpassessor',
-    password: '$2a$08$8g/.DFmReWOtKxE0b4OvneXn5PXttd94GjtS7nJCRl7M4QeNZQoEe',
+    password: '$2a$12$YIctUUQJtNO1aRW8Q2GaNOeq5WYiVdT2EibR6TLj2PgSx73SKpQpa',
     userrole: 'oscpassessor',
   },
   {
     username: 'oscplng',
-    password: '$2a$08$8g/.DFmReWOtKxE0b4OvneXn5PXttd94GjtS7nJCRl7M4QeNZQoEe',
+    password: '$2a$12$YIctUUQJtNO1aRW8Q2GaNOeq5WYiVdT2EibR6TLj2PgSx73SKpQpa',
     userrole: 'oscplindandgrade',
   },
   {
     username: 'oscpstructural',
-    password: '$2a$08$8g/.DFmReWOtKxE0b4OvneXn5PXttd94GjtS7nJCRl7M4QeNZQoEe',
+    password: '$2a$12$YIctUUQJtNO1aRW8Q2GaNOeq5WYiVdT2EibR6TLj2PgSx73SKpQpa',
     userrole: 'oscpstructural',
   },
   {
     username: 'oscparchitectural',
-    password: '$2a$08$8g/.DFmReWOtKxE0b4OvneXn5PXttd94GjtS7nJCRl7M4QeNZQoEe',
+    password: '$2a$12$YIctUUQJtNO1aRW8Q2GaNOeq5WYiVdT2EibR6TLj2PgSx73SKpQpa',
     userrole: 'oscparchitectural',
   },
   {
     username: 'oscpelectrical',
-    password: '$2a$08$8g/.DFmReWOtKxE0b4OvneXn5PXttd94GjtS7nJCRl7M4QeNZQoEe',
+    password: '$2a$12$YIctUUQJtNO1aRW8Q2GaNOeq5WYiVdT2EibR6TLj2PgSx73SKpQpa',
     userrole: 'oscpelectrical',
   },
   {
     username: 'oscpsanitary',
-    password: '$2a$08$8g/.DFmReWOtKxE0b4OvneXn5PXttd94GjtS7nJCRl7M4QeNZQoEe',
+    password: '$2a$12$YIctUUQJtNO1aRW8Q2GaNOeq5WYiVdT2EibR6TLj2PgSx73SKpQpa',
     userrole: 'oscpsanitary',
   },
   {
     username: 'oscpmechanical',
-    password: '$2a$08$8g/.DFmReWOtKxE0b4OvneXn5PXttd94GjtS7nJCRl7M4QeNZQoEe',
+    password: '$2a$12$YIctUUQJtNO1aRW8Q2GaNOeq5WYiVdT2EibR6TLj2PgSx73SKpQpa',
     userrole: 'oscpmechanical',
   }
 ]);
