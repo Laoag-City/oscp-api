e439c219e6b1b7f7d1348a72a0451212d43cf81a
Richard Alhama <richard.alhama@gmail.com>
2024-06-04 04:47:37 +0000

    added token expiration

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,7 +23,7 @@ exports.login = async (req, res) => {
       //return res.status(401).send({ error: 'Login failed!' });
       return res.status(401).json({ message: 'Incorrect username or password' });
     }
-    const token = jwt.sign({ _id: user._id, username:user.username }, secretKey);
+    const token = jwt.sign({ _id: user._id, username:user.username }, secretKey,{expiresIn: "1d"});
     res.send({ _id:user._id, username:user.username, userrole:user.userrole, token });
   } catch (error) {
     res.status(400).send(error);
