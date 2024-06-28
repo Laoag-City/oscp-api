eb9ccb91e1a36293eab419df50887a7cee53f66b
Richard Alhama <richard.alhama@gmail.com>
2024-06-28 04:54:17 +0000

    added logger in authmiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,22 +1,39 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
-
-require('dotenv').config();
+const config = require('../config');
+const logger = require('../utils/logger');
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.header('Authorization').replace('Bearer ', '');
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const authorizationHeader = req.header('Authorization');
+    if (!authorizationHeader) {
+      throw new Error('Authorization header missing');
+    }
+
+    //const token = req.header('Authorization').replace('Bearer ', '');
+
+    const token = authorizationHeader.replace('Bearer ', '');
+    if (!token) {
+      throw new Error('Token missing or malformed');
+    }
+
+    //const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, config.jwtSecret);
     const user = await User.findOne({ _id: decoded._id});
 
+    // if (!user) {
+    //   throw new Error();
+    // }
+
     if (!user) {
-      throw new Error();
+      throw new Error('User not found');
     }
 
     req.token = token;
     req.user = user;
     next();
   } catch (error) {
+    logger.error('Authentication error:', { message: error.message, stack: error.stack });
     res.status(401).send({ error: 'Please authenticate.' });
   }
 };
