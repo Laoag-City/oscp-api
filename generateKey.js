0b473460b806995b38633de89369c32223bafb40
Richard Alhama <richard.alhama@gmail.com>
2024-06-28 04:59:58 +0000

    generateKey.js removed comments

--- generateKey.js
+++ generateKey.js
@@ -10,22 +10,16 @@ const updateEnvFile = (key, filePath = ".env") => {
   const envPath = path.resolve(__dirname, filePath);
   let envContent = "";
 
-  // Check if the .env file exists
   if (fs.existsSync(envPath)) {
     envContent = fs.readFileSync(envPath, "utf8");
-
-    // Remove the existing JWT_SECRET if it exists
     const updatedContent = envContent.replace(/JWT_SECRET=.*\n?/, "");
-
     envContent = updatedContent;
   }
 
-  // Ensure the last line ends with a newline
   if (envContent && !envContent.endsWith("\n")) {
     envContent += "\n";
   }
 
-  // Append the new JWT_SECRET
   envContent += `JWT_SECRET=${key}\n`;
 
   fs.writeFileSync(envPath, envContent);
